bd8a814397e3af1f127ce109b663423c6fa2d67d
Mike Bronner <mike@genealabs.com>
2018-07-17 13:52:00 -0700

    Update ActivatesUsers.php

--- src/Traits/ActivatesUsers.php
+++ src/Traits/ActivatesUsers.php
@@ -1,14 +1,24 @@
 <?php namespace GeneaLabs\LaravelRegistrar\Traits;
 
+use App\User;
 use Illuminate\Http\Request;
 
 trait ActivatesUsers
 {
-    protected function credentials(Request $request)
+    protected function attemptLogin(Request $request)
     {
-        $credentials = $request->only($this->username(), 'password');
-        $credentials['activation_token'] = '';
+        $user = (new User)
+            ->where($this->username(), $request->get($this->username()))
+            ->first();
 
-        return $credentials;
+        if (! $user || ! $user->activated_at) {
+            // TODO: set login failure message
+            return false;
+        }
+
+        return $this->guard()->attempt(
+            $this->credentials($request),
+            $request->filled('remember')
+        );
     }
 }
